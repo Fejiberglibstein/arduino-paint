4b0bcbbafca6f4ef6b526bd05ba9d560a2630cae
Austin Thein <amthein321@gmail.com>
2024-10-16 16:39:13 -0400

    Start on `calculate_point` function

--- lib/proximity.c
+++ lib/proximity.c
@@ -1,8 +1,46 @@
 #include "./proximity.h"
 #include <assert.h>
 #include <math.h>
+#include <sys/types.h>
 
-Point_t calculate_point(Circle_t points[4]) {
+Point_t calculate_point(Circle_t circles[4]) {
+    // We need to get the intersection point(s) between each pair of circles. We
+    // can do this by creating an array of pairs of circles and iterating over
+    // that to find the most common point where the circles overlap
+
+    // TODO: We probably don't need to do this many checks, we could probably
+    // simplify to just adjacent circles?
+    Circle_t patterns[6][2] = {
+        {circles[0], circles[1]},
+        {circles[0], circles[2]},
+        {circles[0], circles[3]},
+        {circles[1], circles[2]},
+        {circles[1], circles[3]},
+        {circles[2], circles[3]},
+    };
+
+    // Array of all possible circle intersection points. We use a count to
+    // determine which point is most common; this will be our overall
+    // intersection point
+    struct {
+        Point_t point;
+        u_int8_t count;
+    } points[12] = {0};
+    uint points_length = 0;
+
+    // Go through all pairs of circles and get the two points of intersection
+    // between the circles
+    for (int i = 0; i < (sizeof(patterns) / sizeof(*patterns)); i++) {
+        Circle_t *pattern = patterns[i];
+
+        Point_t p[2] = {0};
+        circle_intersection(pattern[0], pattern[1], &(p[0]), &(p[1]));
+    }
+
+    // Find the most common point
+    for (int i = 0; i < (sizeof(points) / sizeof(*points)); i++) {
+    }
+
     return (Point_t){0};
 }
 
