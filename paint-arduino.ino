a995d941e547f72f1c107ee4d2c2d45403d11990
Austin Thein <amthein321@gmail.com>
2024-10-24 15:31:20 -0400

    Whoo it blink yay

--- paint-arduino.ino
+++ paint-arduino.ino
@@ -1,6 +1,11 @@
-
 void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+    digitalRead(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalRead(LED_BUILTIN, LOW);
+    delay(1000);
 }
+
