a874df3b1878d13fc0931165f7c93563769b4348
Austin Thein <amthein321@gmail.com>
2024-12-01 09:04:22 -0500

    Main loop done (maybe)?

--- arduino-paint.ino
+++ arduino-paint.ino
@@ -1,14 +1,86 @@
-#include "./proximity.h"
+#include "./point_translation.h"
+// IDK how colors work for this, 
+#define COLOR matrix.Color333(7, 7, 7)
+
+#define CLK  8
+#define OE   9
+#define LAT 10
+#define A   A0
+#define B   A1
+#define C   A2
+
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
+void setup_screen() {
+    matrix.begin();
+}
+
+
+void setup_sensors() {
+    // TODO
+}
+
+Corner corners[4] = {
+    (Corner){
+        // TODO
+        .trigPin = -1,
+        .echoPinLeft = -1,
+        .echoPinRight = -1,
+
+        .x = sensor_coords[0].x,
+        .y = sensor_coords[0].y,
+    },
+    (Corner){
+        // TODO
+        .trigPin = -1,
+        .echoPinLeft = -1,
+        .echoPinRight = -1,
+
+        .x = sensor_coords[1].x,
+        .y = sensor_coords[1].y,
+    },
+    (Corner){
+        // TODO
+        .trigPin = -1,
+        .echoPinLeft = -1,
+        .echoPinRight = -1,
+
+        .x = sensor_coords[2].x,
+        .y = sensor_coords[2].y,
+    },
+    (Corner){
+        // TODO
+        .trigPin = -1,
+        .echoPinLeft = -1,
+        .echoPinRight = -1,
+
+        .x = sensor_coords[3].x,
+        .y = sensor_coords[3].y,
+    }
+}
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+    setup_screen();
+    setup_sensors();
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-    k
+    Circle circles[4] = {0};
+
+    // ping all the corners and get their returned distance into an array of
+    // circles
+    send_ping(corners, &circles);
+
+    // calculate the point from our four circles
+    Point p = calculate_point(circles);
+
+    // Translate the calculate point's coordinates into from world coordinates
+    // to screen coordinates
+    p = scale_point(p)
+
+    // Swapping the x and y here, check the note I left in
+    // `point_translation.cpp`
+    matrix.drawPixel(p.y, p.x, COLOR);
+
 }
 
